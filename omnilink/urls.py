20330799d9a0e45025e60b7969efbf7774ae7906
Don Ekong <officially.ekongg@gmail.com>
2026-08-06 23:16:55 +0100

    Update urls.py

--- omnilink/urls.py
+++ omnilink/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('donekong/', admin.site.urls),
     path('', include('catalog.urls')),
 ]
 
